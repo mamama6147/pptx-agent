6008765cd9707eb684e0a21ec3538425a66fe32e
mamama6147 <116156555+mamama6147@users.noreply.github.com>
2025-09-29 14:37:06 +0900

    Add datamodel.py

---
+++ datamodel.py
@@ -0,0 +1,18 @@
+from langchain_core.pydantic_v1 import BaseModel, Field
+
+
+# ストーリーの評価結果を表すデータモデル
+class Judgement(BaseModel):
+    judge: bool = Field(default=False, description="ストーリーが十分かどうかの判定結果")
+    reason: str = Field(default="", description="ストーリーが十分かどうかの判定理由")
+
+
+# ステートを表すデータモデル
+class State(BaseModel):
+    user_request: str = Field(..., description="ユーザーからのリクエスト")
+    story: str = Field(default="", description="生成されたストーリー")
+    iteration: int = Field(default=0, description="ストーリー生成の反復回数")
+    current_judge: bool = Field(default=False, description="ストーリーが十分かどうかの判定結果")
+    judgement_reason: str = Field(default="", description="ストーリーが十分かどうかの判定理由")
+    slide_contents: str = Field(default="", description="スライドの内容")
+    slide_gen_code: str = Field(default="", description="スライド生成のコード")
